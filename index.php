6c64698195ddf528966f6381089cc00eaeece5e0
root <robbiebinnie@gmail.com>
2018-09-21 09:45:53 +0000

    user stuff added

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ echo "<br/><br/>";
 
 foreach (getallheaders() as $name => $value) {
     if ($name == "X-MS-CLIENT-PRINCIPAL-NAME"){
-        echo "$name: $value <br/>";
+        echo "hello, $value i can see your datas <br/>";
     }
 }
 
