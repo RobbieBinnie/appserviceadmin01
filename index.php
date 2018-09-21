61adb7f2d71e1bc993f52347e1d72f5807601b7a
root <robbiebinnie@gmail.com>
2018-09-21 09:49:29 +0000

    text changed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "<h1>hello authenticated people!</h1>";
-echo "this is where you should order...<br/>";
+echo "this is where you should do admin stuff...<br/>";
 
 echo $_SERVER['X-MS-CLIENT-PRINCIPAL-NAME'];
 
@@ -10,7 +10,7 @@ echo "<br/><br/>";
 
 foreach (getallheaders() as $name => $value) {
     if ($name == "X-MS-CLIENT-PRINCIPAL-NAME"){
-        echo "hello, $value i can see your datas <br/>";
+        echo "hello, $value i can see your datas in my directory <br/>";
     }
 }
 
