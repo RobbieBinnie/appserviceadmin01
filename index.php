023d5088e98d71dce6793826796c3c8a6d6b03a3
root <robbiebinnie@gmail.com>
2018-09-21 09:33:45 +0000

    added loop for headers

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 echo "<h1>hello authenticated people!</h1>";
 echo "this is where you should order...";
 
-echo $_SERVER['REMOTE_USER'];
+//echo $_SERVER['REMOTE_USER'];
+
+foreach (getallheaders() as $name => $value) {
+    echo "$name: $value\n";
+}
+
+
 
 ?>
