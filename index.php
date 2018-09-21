58ecb4e8f61d0f60aeb3dfbd086fea4592db86c7
root <robbiebinnie@gmail.com>
2018-09-21 09:37:50 +0000

    added new header read

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 
 echo "<h1>hello authenticated people!</h1>";
-echo "this is where you should order...";
+echo "this is where you should order...<br/>";
 
-//echo $_SERVER['REMOTE_USER'];
+echo $_SERVER['X-MS-CLIENT-PRINCIPAL-NAME'];
+
+echo "<br/><br/>";
+
 
 foreach (getallheaders() as $name => $value) {
     echo "$name: $value\n";
